55c9b145b0e7b4a5824b9eabde599168fe016a63
edo-showwcase <adityawarmanfw@gmail.com>
2024-01-16 21:17:16 +0700

    404

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,9 +1,15 @@
 <!-- This page handles any error encountered by the site. -->
 <script>
-import { page } from '$app/stores'
+	import { page } from '$app/stores';
 </script>
 
 <h2>{$page.status}</h2>
-<p class="subhead">{$page.error.message}</p>
+<p class="py-4 text-xl">
+	{#if $page.status === 404}
+		Halaman tidak ditemukan.
+	{:else}
+		An unexpected error occurred.
+	{/if}
+</p>
 
 <p><strong>Maaf!</strong> Coba kembali ke <a href="/">laman awal</a></p>
---
+++ src/routes/404/+page.svelte
@@ -0,0 +1,5 @@
+<h2>404</h2>
+
+<p class="py-4 text-xl">Halaman tidak ditemukan.</p>
+
+<p><strong>Maaf!</strong> Coba kembali ke <a href="/">laman awal</a></p>
